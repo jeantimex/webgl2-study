aad42c714f99b4a1941539d52158080ff50cdee7
Yong Su <jean.timex@gmail.com>
2022-10-31 23:29:35 -0700

    add linting

--- src/common/webgl-util.ts
+++ src/common/webgl-util.ts
@@ -4,7 +4,7 @@ interface WebGLSetupOptions {
   fragmentShaderSource: string;
   width?: string;
   height?: string;
-  draw: Function;
+  draw: (gl: WebGL2RenderingContext) => void;
 }
 
 /**
@@ -65,8 +65,8 @@ export function setUpWebGL(options: WebGLSetupOptions) {
   gl.linkProgram(program);
 
   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.log(gl.getShaderInfoLog(vertexShader));
-    console.log(gl.getShaderInfoLog(fragmentShader));
+    console.error(gl.getShaderInfoLog(vertexShader));
+    console.error(gl.getShaderInfoLog(fragmentShader));
   }
 
   gl.useProgram(program);
